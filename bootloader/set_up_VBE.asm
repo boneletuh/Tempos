04a47be177daac8fc52fccb46fec77f9d10bf663
boneletuh <145162900+boneletuh@users.noreply.github.com>
2024-10-27 10:49:51 +0100

    little improvement to bootloader
    only use video modes with linear frame buffer

--- bootloader/set_up_VBE.asm
+++ bootloader/set_up_VBE.asm
@@ -13,42 +13,58 @@ int 10h
 mov ax, WORD [VBE_VesaInfoBlock_pointer + VesaInfoBlock.VideoModesSegment]
 mov es, ax
 mov bx, WORD [VBE_VesaInfoBlock_pointer + VesaInfoBlock.VideoModesOffset]
+
+mov ax, 4F01h ; VBE mode information function
+mov di, VBE_ModeInfoBlock_pointer ; address where the info will be stored
 .match_video_mode:
-    mov dx, WORD [es:bx]
+    mov cx, WORD [es:bx]
 
-	cmp dx, 011Bh	; 1280×1024x24bpp
+	; get the information about the desired video mode
+	; di - address where the info will be stored
+	; cx - video mode number
+	; ax - 4F01h
+	int 10h
+	; FIX: panic if returned error code in ax is not 0x004f
+	mov dx, WORD [VBE_ModeInfoBlock_pointer + VesaModeInfoBlock.ModeAttributes]
+	and dx, 1<<7 ; check if the mode has a linear/flat frame buffer
+	cmp dx, 0
+	je .match_video_mode_keep_searching ; if it doesnt have linear buffer skip this mode
+
+	cmp cx, 011Bh	; 1280×1024x24bpp
 	je .match_video_mode_end
 
-	cmp dx, 0118h	; 1024×768x24bpp
+	cmp cx, 0118h	; 1024×768x24bpp
 	je .match_video_mode_end
 
-	cmp dx, 0115h	; 800×600x24bpp
+	cmp cx, 0115h	; 800×600x24bpp
 	je .match_video_mode_end
 
-	cmp dx, 0112h	; 640x480x24bpp
+	cmp cx, 0112h	; 640x480x24bpp
 	je .match_video_mode_end
+
+.match_video_mode_keep_searching:
 
     add bx, 2 ; each entry in the VideoModeList is 16 bit
 
-    cmp dx, 0xffff
+    cmp cx, 0xffff
     jne .match_video_mode
-; if not match was found panic
-; FIX: call panic
+; FIX: if no match was found: panic
 mov dx, 0x0BAD
 call print_hex
 jmp $
+
 .match_video_mode_end:
 
 ; get the information about the desired video mode
 mov di, VBE_ModeInfoBlock_pointer ; address where the info will be stored
-mov cx, dx ; video mode number
-mov ax, 4F01h ; VBE mode information
+; cx - video mode number
+mov ax, 4F01h ; VBE mode information function
 int 10h
 ; FIX: panic if returned error code in ax is not 0x004f
 
 ; set the VBE video mode
 ; es:di is ignored because bx[11] is set to 0
-mov bx, dx
-or bx, 0x4000 ; | (1 << 14) ; use linear/flat frame buffer
+mov bx, cx
+or bx, 1<<14 ; use linear/flat frame buffer
 mov ax, 4F02h ; set video mode
 int 10h
